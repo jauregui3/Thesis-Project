9c200c9ddd074ca96ab06e0a9089eeaec130a23d
Juancarlos Jauregui <jauregui3@gmail.com>
2017-07-13 20:18:33 -0700

    Add server

--- public/src/game.js
+++ public/src/game.js
@@ -14,10 +14,10 @@ Game.prototype = {
         this.game.world.setBounds(-width, -height, width*2, height*2);
         // this.game.stage.backgroundColor = '#d3d3d3';
         var background = this.game.add.tileSprite(-width, -height, this.game.world.width, this.game.world.height, 'background');
-        this.game.camera.height = 500;
-        this.game.camera.width = 500;
 
-        this.game.camera.position = new Phaser.Point(1000, 1000);
+        // this.game.camera.setPosition(500, 500);
+        // this.game.camera.setSize(1000, 1000)
+
     },
     /**
      * Main update loop
