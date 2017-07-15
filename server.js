670e400bee79de268d47cd73d1a3138ec69892f1
david <david@davidberry.io>
2017-07-15 14:10:17 -0700

    socket

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 8081;
+
 
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
-console.log('listening on port >>>>' + port);
+server.listen(port, function () {
+  console.log('listening on port >>>>' + port);
+});
+
+io.on('connection', function (socket) {
+
+  socket.on('clientUpdate', function (data) {
+    console.log('client update',data);
+  });
+
+});
