7be500bea6f1174a16e80ef1bf5af4ff723f926c
SeanBrock <biker1070@gmail.com>
2017-07-15 16:00:17 -0500

    require chai and mocha, add client/server spec tests, and intentionally break Travis by failing tests.

---
+++ test/server/ServerSpec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+//remember to require files for testing here
+
+describe('server tests', function() {
+  it('should pass a sanity test', function() {
+    expect(true).to.be.true;
+  });
+});
