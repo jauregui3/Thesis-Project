11c5bf397308f5ed44896ee9c378b3048f82915f
david <david@davidberry.io>
2017-07-31 15:07:20 -0700

    testing suite for server

--- server.js
+++ server.js
@@ -1,90 +1,87 @@
+// droplet IP address: 138.68.251.126
 require('dotenv').config();
 var express = require('express');
 var app = express();
+//var server = require('http').createServer();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
 var port = process.env.PORT || 8081;
-
 app.use(express.static(__dirname + '/public'));
 
-// ----------------------------------------------------------------
-// we need to update this section with exactly what redis is doing
-
-var redis;
 if (process.env.REDISTOGO_URL) {
-  var rtg = require('url').parse(process.env.REDISTOGO_URL);
-  redis = require('redis').createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(':')[1]);
+  console.log('theres a redis url, here we go');
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+
 } else {
-  redis = require('redis').createClient();
+  var redis = require("redis").createClient();
 }
-
-// misspelling: scoreobard?
-redis.zincrby('scoreobard', 2, 'dave');
-var scoreboard1 = redis.zrangebyscore('scoreboard', 0, 999999, 'WITHSCORES', scoreboardCallback);
-
-redis.zincrby('scoreboard', 1, 'dave');
-var scoreboard2 = redis.zrangebyscore('scoreboard', 0, 999999, 'WITHSCORES', scoreboardCallback);
-
 var scoreboardCallback = function(err, response) {
-  if (err) {
-    console.error(err);
-  }
-};
+  if (err) {console.error(err);}
 
-// ----------------------------------------------------------------
+  //console.log('scoreboard callback: ', response);
+  // console.log(response);
+}
+// redis.zadd("scoreboard", 1, "juancarlos");
+// redis.zadd("scoreboard", 0, "dave");
+redis.zincrby("scoreobard", 2, "dave");
+var scoreboard1 = redis.zrangebyscore("scoreboard", 0, 999999, "WITHSCORES", scoreboardCallback);
+
+redis.zincrby("scoreboard", 1, "dave");
+var scoreboard2 = redis.zrangebyscore("scoreboard", 0, 999999, "WITHSCORES", scoreboardCallback);
+
 var players = [];
 
-function Player (id, nickName) {
+
+function Player (id) {
   this.id = id;
   this.x = 0;
   this.y = 0;
   this.z = 0;
-  this.nickName = nickName || '';
   this.entity = null;
   this.lastCollision = null;
 }
 
 io.sockets.on('connection', function(socket) {
-  console.log('connection event fired line 50 hi dave');
-  socket.on('initialize', function(data) {
+  //console.log('new connection', players.length);
+  socket.on('initialize', function() {
     var idNum = players.length;
-    var nickName = data.nickName;
-    var newPlayer = new Player (idNum, nickName);
+    var newPlayer = new Player (idNum);
 
     players.push(newPlayer);
-    console.log('about to emit playerData');
-    socket.emit('playerData', {id: idNum, nickName: nickName, players: players});
+    socket.emit('playerData', {id: idNum, players: players});
 
     socket.broadcast.emit('playerJoined', newPlayer);
   });
-  socket.on('deletePlayer', function(id) {
-    //remove the player from players
-    //socket.broadcast.emit('deleteOther', {id:id, players:players})
-    players[id] = 'dead'; // -1 may be wrong but testing....
-  })
+
   socket.on('positionUpdate', function(data) {
     var dataKeys = Object.keys(data);
+    //console.log(this, dataKeys);
     dataKeys.map(function(curKey) {
       if (curKey !== 'id') {
+        //console.log(data.id, players[data.id], curKey);
         players[data.id][curKey] = data[curKey];
       }
     });
     /*
-      players[data.id].x = data.x;
-      players[data.id].y = data.y;
-      players[data.id].z = data.z;
-      players[data.id].vx: lv.x,
-      players[data.id.vy: lv.y,
-      players[data.id].vz: lv.z,
-      players[data.id].ax: av.x,
-      players[data.id].ay: av.y,
-      players[data.id].az: av.z
+    players[data.id].x = data.x;
+    players[data.id].y = data.y;
+    players[data.id].z = data.z;
+    players[data.id].vx: lv.x,
+    players[data.id.vy: lv.y,
+    players[data.id].vz: lv.z,
+    players[data.id].ax: av.x,
+    players[data.id].ay: av.y,
+    players[data.id].az: av.z
     */
     socket.broadcast.emit('playerMoved', data);
   });
 });
 
-console.log('server running on port: ', port);
+console.log('server running');
 server.listen(port);
+
+exports.players = players;
+exports.Player = Player;
+exports.app = app;
