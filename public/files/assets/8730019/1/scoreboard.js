bb78a89571452afc11ed1f167c8792a09fc678c6
James Quillin <jamesquillin1@gmail.com>
2017-08-04 17:08:15 -0700

    Update scoreboard.js

--- public/files/assets/8730019/1/scoreboard.js
+++ public/files/assets/8730019/1/scoreboard.js
@@ -32,56 +32,44 @@ Scoreboard.prototype.initialize = function() {
     //                                           ]
   window.socket.on('leaderboardUpdate', function(scoreData) {   
       
-      var n = Math.min(10, scoreData.length);
-      var temp, id, nickName, points;
-      var scoreList = document.getElementById('score-list');
-      scoreList.innerHTML = '';
-      var scoreRank = document.getElementById('score-rank');
+    var n = Math.min(10, scoreData.length);
+    var temp, id, nickName, points;
+    var scoreList = document.getElementById('score-list');
+    scoreList.innerHTML = '';
+    var scoreRank = document.getElementById('score-rank');
       
-      for (var i = 0; i < n; i++) {
-          temp = scoreData[i].split(' ');
-          id = temp[0];
-          nickName = temp[1];
+    for (var i = 0; i < n; i++) {
+      temp = scoreData[i].split(' ');
+      id = temp[0];
+      nickName = temp[1];
           
-          if (nickName !== undefined) {
-              if (nickName.length > 10) {
-                  nickName = nickName.split('').slice(0, 8).join('');
-                  nickName += '...';
-              }
-              points = scoreData[i+1];
+      if (nickName !== undefined) {
+        if (nickName.length > 10) {
+          nickName = nickName.split('').slice(0, 8).join('');
+          nickName += '...';
+        }
+        points = scoreData[i+1];
 
-              var newLI = document.createElement('li');
-              newLI.innerHTML = `<span style="display: inline-block; overflow: hidden;">#${Math.ceil((i+1)/2)} | ${nickName}</span>
-                                 <span style="display: inline-block; float: right;">${points}</span>`;
-              scoreList.appendChild(newLI);
-              // if (window.player.id + '' === id) {
-              //     console.log(scoreData);
-              //     var myScore = scoreData[i + 1];
-              //     var rank = Math.ceil((i+1)/2);
-              //     scoreRank.innerHTML = '';
-              //     var pTag = document.createElement('p');
-              //     pTag.innerHTML = `Your score: ${myScore}</p><p>Your rank: ${rank}`;
-              //     scoreRank.appendChild(pTag);
-              // }
-          }
-          
-          for (var j = 0; j < scoreData.length; j++) {
-            var cur = scoreData[i].split(' ');
-            var redisId = cur[0];
-              if (nickName !== undefined) {
-                  if (window.player.id + '' === redisId) {
-                      console.log(scoreData);
-                      var myScore = scoreData[i + 1];
-                      var rank = Math.ceil((i+1)/2);
-                      scoreRank.innerHTML = '';
-                      var pTag = document.createElement('p');
-                      pTag.innerHTML = `Your score: ${myScore}</p><p>Your rank: ${rank}`;
-                      scoreRank.appendChild(pTag);
-                  } 
-              }
-          }
+        var newLI = document.createElement('li');
+        newLI.innerHTML = `<span style="display: inline-block; overflow: hidden;">#${Math.ceil((i+1)/2)} | ${nickName}</span>
+                           <span style="display: inline-block; float: right;">${points}</span>`;
+        scoreList.appendChild(newLI);
       }
-      
-      // If we can get something like line 72 working, we can do the rest of this..
+          
+      for (var j = 0; j < scoreData.length; j++) {
+        var cur = scoreData[i].split(' ');
+        var redisId = cur[0];
+        if (nickName !== undefined) {
+          if (window.player.id + '' === redisId) {
+            var myScore = scoreData[i + 1];
+            var rank = Math.ceil((i+1)/2);
+            scoreRank.innerHTML = '';
+            var pTag = document.createElement('p');
+            pTag.innerHTML = `Your score: ${myScore}</p><p>Your rank: ${rank}`;
+            scoreRank.appendChild(pTag);
+          } 
+        }
+      }
+    }
   });
 };
